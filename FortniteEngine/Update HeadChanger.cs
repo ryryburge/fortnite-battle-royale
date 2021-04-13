55bb1eb2cfd3b0c9c0ebe88e7d34c667ae54adb5
ryryburge <82178482+ryryburge@users.noreply.github.com>
2021-04-13 16:35:24 +0200

    Create Update HeadChanger.cs

---
+++ FortniteEngine/Update HeadChanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+@@ -10,7 +10,7 @@ public class HeadChanger : MonoBehaviour
+{
+    public GameObject Head;
+    public Sprite image;
+   public  string url;
+
+
+    // Start is called before the first frame update
+    void Start()
+@@ -29,6 +29,11 @@ public void Htesting()
+
+
+    }
+
+    public void shit
+    {
+
+    }
+
+
+
